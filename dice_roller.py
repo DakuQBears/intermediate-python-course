79346d5f29f83538da1645fafd2542ded57f3140
DakuQBears <oldfiesa@gmail.com>
2020-06-07 15:08:06 -0400

    Added user input for dice amount and dice type

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 import random as rnd
 
 def main():
-  dice_rolls = 2
+  dice_rolls = int(input('How many dice would you like to roll? '))
+  dice_size = int(input('How many sides are the dice? '))
   dice_sum = 0
   for i in range(0,dice_rolls):
-    roll = rnd.randint(1,6)
+    roll = rnd.randint(1,dice_size)
     dice_sum += roll
-    print(f'You rolled a {roll}')
+    if roll == 1:
+      print(f'You rolled a {roll}. Critical fail!')
+    elif roll == dice_size:
+      print(f'You rolled a {roll}! Critical Success!')
+    else:
+      print(f'You rolled a {roll}')
 
   print(f'The sum of the dice is {dice_sum}')
   
